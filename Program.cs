95a6523c08e15ca6697e0af4eea176140eb85f69
Matthew C. Lind <overboardmediagroup@mail.com>
2022-09-13 08:39:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, at {currentDate:t} on {currentDate:d}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit {name}...");
+            Console.ReadKey(true);
+        }
+    }
+}
